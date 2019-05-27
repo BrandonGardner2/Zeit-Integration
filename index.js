d7df19bb02ac0356d47d7f8b953a400a7ceeb8f8
Brandon G-Home <brandon.gardner2@outlook.com>
2019-05-27 12:56:17 -0500

    Changes to metadata store

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ const { withUiHook } = require("@zeit/integration-utils");
 
 let count = 0;
 
-module.exports = withUiHook(({ payload }) => {
-  count += 1;
+module.exports = withUiHook(async ({ payload, zeitClient }) => {
+  const metadata = await zeitClient.getMetadata();
+  metadata.count = metadata.count || 0;
+  metadata.count += 1;
+  await zeitClient.setMetadata(metadata);
+
   return `
     <Page>
-        <P>Counter: ${count}</P>
+        <P>Counter: ${metadata.count}</P>
         <Button>Count Me</Button>
     </Page>
     `;
